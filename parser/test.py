9f1dcea7b1b3e762712902c696d1710d02221c38
Arun Dunna <adunna@cs.umass.edu>
2020-12-17 00:46:22 -0500

    update

--- parser/test.py
+++ parser/test.py
@@ -2,7 +2,7 @@ from mparser import MatrixParser
 from manalyzer import MatrixAnalyzer
 
 parseEngine = MatrixParser()
-game = parseEngine.readLog("../samples/map1.txt")
+game = parseEngine.readLog("../samples/Hollywood.txt")
 analyzer = MatrixAnalyzer(game)
 
 #owssparser.write_csv(parsed_data, "../samples/workshop_output1.csv")
