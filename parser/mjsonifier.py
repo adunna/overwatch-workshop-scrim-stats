7c8bd76cf24808ee9e1883f72a0d558cfa8bb2c7
adunna <adunna@umass.edu>
2020-12-21 05:36:09 -0500

    update

---
+++ parser/mjsonifier.py
@@ -0,0 +1,13 @@
+from mparser import MatrixParser
+from manalyzer import MatrixAnalyzer
+
+class MatrixJSON:
+    
+    def __init__(self, logfile):
+        self.ParseEngine = MatrixParser()
+        self.game = self.ParseEngine.readLog(logfile)
+        self.Analyzer = MatrixAnalyzer(self.game)
+
+    def DumpJSON(self):
+        players = self.Analyzer.GetPlayers()
+        return players
