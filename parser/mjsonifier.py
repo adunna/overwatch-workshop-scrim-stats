12fb6f08ef71cb681207369517c0959cc9d04090
Arun Dunna <adunna@cs.umass.edu>
2020-12-25 20:22:03 -0500

    add time to ult and time ult held to json

--- parser/mjsonifier.py
+++ parser/mjsonifier.py
@@ -5,7 +5,7 @@ from manalyzer import MatrixAnalyzer
 STAT_TYPES = ['all_damage_dealt', 'barrier_damage_dealt', 'hero_damage_dealt', 'damage_blocked', 'damage_taken', 'eliminations', 'deaths', 'environmental_deaths', 'environmental_kills', 'final_blows', 'healing_dealt', 'healing_received', 'objective_kills', 'solo_kills', 'ultimate_charge', 'ultimates_earned', 'ultimates_used', 'cooldown1', 'cooldown2']
 
 class MatrixJSON:
-    
+
     def __init__(self, logfile):
         self.ParseEngine = MatrixParser()
         self.game = self.ParseEngine.readLog(logfile)
@@ -16,6 +16,8 @@ class MatrixJSON:
         final_stats = {player: {stat: self.Analyzer.GetFinalStat(player, stat, players[player]) for stat in STAT_TYPES} for player in players}
         stats_per_minute = {player: {stat: self.Analyzer.GetStatPerMinute(player, stat, players[player]) for stat in STAT_TYPES} for player in players}
         heroes_played = {player: self.Analyzer.GetHeroesPlayed(player) for player in players}
+        avg_time_to_ult = {player: self.Analyzer.GetAverageTimeToUltimate(player) for player in players}
+        avg_time_ult_held = {player: self.Analyzer.GetAverageTimeUltimateHeld(player) for player in players}
         team_damage_over_time = {
                 "team1": self.Analyzer.GetAllTotalDamages(team=0),
                 "team2": self.Analyzer.GetAllTotalDamages(team=1),
@@ -26,4 +28,6 @@ class MatrixJSON:
                 "final_stats": final_stats,
                 "stats_per_minute": stats_per_minute,
                 "team_damage_over_time": team_damage_over_time,
+                "avg_time_to_ult": avg_time_to_ult,
+                "avg_time_ult_held": avg_time_ult_held,
                 }
--- parser/test.py
+++ parser/test.py
@@ -3,15 +3,15 @@ from manalyzer import MatrixAnalyzer
 from mjsonifier import MatrixJSON
 import pprint
 
-#MJ = MatrixJSON("../samples/Hollywood.txt")
-#pprint.pprint(MJ.DumpJSON())
+MJ = MatrixJSON("../samples/i0yY2MOFe5WreEb4.txt")
+pprint.pprint(MJ.DumpJSON()['heroes_played'])
 
 parseEngine = MatrixParser()
 game = parseEngine.readLog("../samples/i0yY2MOFe5WreEb4.txt")
 analyzer = MatrixAnalyzer(game)
 #print(analyzer.GetAllTotalDamages())
 #print(analyzer.game.section_lengths)
-print({player: analyzer.GetHeroesPlayed(player) for player in analyzer.GetPlayers()})
+#print({player: analyzer.GetHeroesPlayed(player) for player in analyzer.GetPlayers()})
 #parseEngine.write_csv(game, "../samples/TEST.csv")
 
 #owssparser.write_csv(parsed_data, "../samples/workshop_output1.csv")
